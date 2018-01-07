b170e1ddf5568c3240568be5154732b8ffc4d0d2
Guillaume Campagna <guillaume.campagna@gmail.com>
2018-01-07 17:02:25 -0500

    Snake case, sigh

--- plugins/clubhouse.rb
+++ plugins/clubhouse.rb
@@ -15,61 +15,61 @@ require "clubhouse"
 Clubhouse.default_client = Clubhouse::Client.new(ENV['CLUBHOUSE_TOKEN'])
 project = $omnifocus.flattened_projects["Clubhouse"].get
 
-myUserUUID = nil
+user_uuid = nil
 Clubhouse::User.all.each do |user|
   if user.username == ENV['CLUBHOUSE_USERNAME']
     puts 'User id for ' + ENV['CLUBHOUSE_USERNAME'] + ' is ' + user.id
-    myUserUUID = user.id
+    user_uuid = user.id
   end
 end
 
-if myUserUUID == nil
+if user_uuid == nil
   throw 'Couldn\'t find user. Check that CLUBHOUSE_USERNAME is correct'
 end
 
-workflowStateIdsToShow = []
+workflow_state_ids_to_show = []
 Clubhouse::Workflow.all.each do |workflow|
-  workflow.states.each do |workflowState|
-    if ['unstarted', 'started'].include? workflowState.type
-      workflowStateIdsToShow.push(workflowState.id)
+  workflow.states.each do |workflow_state|
+    if ['unstarted', 'started'].include? workflow_state.type
+      workflow_state_ids_to_show.push(workflow_state.id)
     end
   end
 end
 
-if workflowStateIdsToShow.empty?
+if workflow_state_ids_to_show.empty?
   throw 'Couldn\'t find some valid workflow step to shows.'
 end
 
-storyIdentifier = []
+story_identifier = []
 
-stories = Clubhouse::Story.search(owner_id: myUserUUID)
+stories = Clubhouse::Story.search(owner_id: user_uuid)
 stories.each do |story|
-  projectName = Clubhouse::Project.find(story.project_id).name
-  epicName = Clubhouse::Epic.find(story.epic_id).name rescue "No Epic"
-  storyId = "[%s-%s #%d]" % [projectName, epicName, story.id]
-  storyUrl = "https://app.clubhouse.io//story/" + story.id.to_s
-  storyShouldBeShown = workflowStateIdsToShow.include? story.workflow_state_id
+  project_name = Clubhouse::Project.find(story.project_id).name
+  epic_name = Clubhouse::Epic.find(story.epic_id).name rescue "No Epic"
+  story_id = "[%s-%s #%d]" % [project_name, epic_name, story.id]
+  story_url = "https://app.clubhouse.io//story/" + story.id.to_s
+  story_should_be_shown = workflow_state_ids_to_show.include? story.workflow_state_id
 
-  storyIdentifier.push(storyId)
+  story_identifier.push(story_id)
 
-  task = project.tasks[its.name.contains(storyId)].first.get rescue nil
+  task = project.tasks[its.name.contains(story_id)].first.get rescue nil
   if task
-    if storyShouldBeShown && task.completed.get
-      puts 'Uncompleting in OmniFocus: ' + storyId
+    if story_should_be_shown && task.completed.get
+      puts 'Uncompleting in OmniFocus: ' + story_id
       task.completed.set false
-    elsif !storyShouldBeShown && !task.completed.get
-      puts 'Completing in OmniFocus: ' + storyId
+    elsif !story_should_be_shown && !task.completed.get
+      puts 'Completing in OmniFocus: ' + story_id
       task.completed.set true
     else
-      puts 'Updating in OmniFocus: ' + storyId
-      update_if_changed task, :note, storyUrl
-      update_if_changed task, :name, "%s %s" % [storyId, story.name]
+      puts 'Updating in OmniFocus: ' + story_id
+      update_if_changed task, :note, story_url
+      update_if_changed task, :name, "%s %s" % [story_id, story.name]
     end
-  elsif storyShouldBeShown
-    puts 'Adding: ' + storyId
+  elsif story_should_be_shown
+    puts 'Adding: ' + story_id
     project.make :new => :task, :with_properties => {
-      :name => "%s %s" % [storyId, story.name],
-      :note => storyUrl,
+      :name => "%s %s" % [story_id, story.name],
+      :note => story_url,
     }
   end
 end
@@ -81,7 +81,7 @@ project.tasks().get.each do |task|
     if matches && matches.size == 2
       story_id = matches["identifier"]
 
-      if !storyIdentifier.include? story_id
+      if !story_identifier.include? story_id
         puts "Removing task #{story_id} in OmniFocus because assignment was removed"
         $omnifocus.delete task
       end
